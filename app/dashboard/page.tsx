982206d9345dd6d74ad3d5136b9e20a4a7b3afbe
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2025-06-26 10:42:09 -0400

    Protected dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,22 @@
 'use client'
 
+import { useRouter } from "next/navigation";
+
+import { useSession } from "next-auth/react"
+
 export default function Dashboard() {
+    const { data: session, status } = useSession();
+    const router = useRouter();
+
+    if (status === "loading") {
+        return <p>Loading</p>;
+    }
+
+    if (status === "unauthenticated") {
+        router.replace("/api/auth/signin");
+        return <p>403 | Log in to see this page</p>;
+    }
+
     return (
         <>
         
