aef50c2c31d22458212c9b9c43225305c10ae6bd
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2025-12-03 17:17:35 -0500

    Made basic nav bar

---
+++ app/navbar.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem } from "@heroui/navbar";
+import { Link } from "@heroui/link";
+import { Button } from "@heroui/button";
+
+export function Nav() {
+    return (
+        <Navbar>
+            <NavbarBrand>
+                Time Tracker V2
+            </NavbarBrand>
+
+            <NavbarContent justify="center">
+                <NavbarItem>
+                    <Link href="/">Home</Link>
+                </NavbarItem>
+
+                <NavbarItem>
+                    <Link href="/dashboard">Dashboard</Link>
+                </NavbarItem>
+
+                <NavbarItem>
+                    <Link href="/projects">Projects</Link>
+                </NavbarItem>
+            </NavbarContent>
+
+            <NavbarContent justify="end">
+                <Button onPress={() => {window.location.herf="/api/auth/signin"}}>Signin/Signup</Button>
+            </NavbarContent>
+        </Navbar>
+    );
+};
