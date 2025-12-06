ca784b72b548efdc123bd71b63687cfcf5884dbf
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2025-12-05 20:23:29 -0500

    Improved project color styling

--- app/projects/ProjectRow.tsx
+++ app/projects/ProjectRow.tsx
@@ -6,14 +6,22 @@ import { EditButton, DeleteButton } from "./projectActions";
 
 import { DatabaseProjectsTable } from "@/type";
 
+import styles from "../styles/projects/projectRow.module.css";
+
 export function ProjectRows(props: any) {
     const projects: DatabaseProjectsTable[] = props.projects;
 
     return (
         <TableBody>
             {projects.map((project: DatabaseProjectsTable) => (
-                <TableRow key={project.id} style={{backgroundColor: project["color"]}}>
-                    <TableCell>{project.name}</TableCell>
+                <TableRow key={project.id}>
+                    <TableCell>
+                        <ul>
+                            <li style={{color: project["color"]}} className={`${styles.projectListItem}`}>
+                                <span className={`${styles.projectName}`}>{project.name}</span>
+                            </li>
+                        </ul>
+                    </TableCell>
                     <TableCell>{project.description}</TableCell>
                     <TableCell>
                         <EditButton project={project} />
